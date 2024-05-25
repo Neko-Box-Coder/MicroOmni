9b4ecf05d395d5876fc52d0283a93c9f9043fa7a
Neko Box Coder <nekoboxcoder@gmail.com>
2024-05-25 21:16:29 +0100

    Updating find content for prompts and readme

--- MicroOmni.lua
+++ MicroOmni.lua
@@ -10,12 +10,23 @@ local util = import("micro/util")
 local screen = import("micro/screen")
 local runtime = import("runtime")
 
-local OmniContentArgs =  config.GetGlobalOption("OmniContentArgs")
+
+local os = import("os")
+
+
+
+
+-- local OmniContentInputArgs = config.GetGlobalOption("OmniContentInputArgs")
+local OmniContentArgs = config.GetGlobalOption("OmniContentArgs")
 local OmniSelectType = config.GetGlobalOption("OmniSelectType")
 
 local fzfCmd =  config.GetGlobalOption("fzfcmd")
 local fzfOpen = config.GetGlobalOption("fzfopen")
-local fzfPath = config.GetGlobalOption("fzfpath")
+
+
+local OmniContentFindPath = ""
+local OmniSearchText = ""
+-- local fzfPath = config.GetGlobalOption("fzfpath")
 
 
 
@@ -28,16 +39,8 @@ function getOS()
 end
 
 function setupFzf(bp)
-
     if fzfCmd == nil then
         fzfCmd = "fzf"
-    end
-
-    if fzfPath == "relative" then
-        currentdir = filepath.Dir(bp.buf.Path)
-        bp:CdCmd({currentdir})
-    elseif fzfPath ~= nil and fzfPath ~= "" then
-        bp:CdCmd(filepath(fzfPath))
     end
 
     if fzfOpen == nil then
@@ -46,54 +49,114 @@ function setupFzf(bp)
 end
 
 function OmniContent(bp)
-    if OmniContentArgs == nil then
-        OmniContentArgs = ""
+    if OmniContentArgs == nil or OmniContentArgs == "" then
+        -- OmniContentArgs =   "--bind 'start:reload:rg -i -uu -n {q}' "..
+        OmniContentArgs =   "--bind 'alt-f:reload:rg -i -uu -n {q}' "..
+                            "--delimiter : -i "..
+                            "--bind page-up:preview-half-page-up,page-down:preview-half-page-down,"..
+                            "alt-up:half-page-up,alt-down:half-page-down "..
+                            "--preview-window '+{2}-/2' "..
+                            "--preview 'bat -f -n --highlight-line {2} {1}'"
     end
 
-    local selectedText = ""
+    OmniSearchText = ""
     if bp.Cursor:HasSelection() then
-        selectedText = bp.Cursor:GetSelection()
+        OmniSearchText = bp.Cursor:GetSelection()
+        OmniSearchText = util.String(OmniSearchText)
+        micro.InfoBar():Prompt("Search Directory ({fileDir}) > ", "", "", nil, OnSearchDirSetDone)
     else
-        -- micro.InfoBar():Error("You must select something first before searching anything")
-        micro.InfoBar():Prompt("Content to find > ", "", "", nil, OnFindPromptDone)
-        return
+        micro.InfoBar():Prompt("Search Directory ({fileDir}) > ", "", "", nil, OnSearchDirSetDone)
     end
-
-    FindContent(selectedText)
 end
 
-function OnFindPromptDone(content, cancelled)
+function OnSearchDirSetDone(resp, cancelled)
     if cancelled then return end
-    FindContent(content)
+
+    local bp = micro.CurPane()
+    if bp == nil then return end
+    
+    OmniContentFindPath = resp:gsub("{fileDir}", filepath.Dir(bp.buf.Path))
+
+    if OmniSearchText == "" then
+        micro.InfoBar():Prompt("Content to find > ", "", "", nil, OnFindPromptDone)
+    else
+        FindContent(OmniSearchText, OmniContentFindPath)
+    end
 end
 
-function FindContent(str)
+function OnFindPromptDone(resp, cancelled)
+    if cancelled then return end
+    FindContent(resp, OmniContentFindPath)
+    return
+end
+
+-- Grabbed from filemanager2
+-- Stat a path to check if it exists, returning true/false
+function path_exists(path)
+    -- Stat the file/dir path we created
+    -- file_stat should be non-nil, and stat_err should be nil on success
+    local file_stat, stat_err = os.Stat(path)
+    -- Check if what we tried to create exists
+    if stat_err ~= nil then
+        -- true/false if the file/dir exists
+        return os.IsExist(stat_err)
+    elseif file_stat ~= nil then
+        -- Assume it exists if no errors
+        return true
+    end
+    return false
+end
+
+function IsPathDir(path)
+    -- Stat the file/dir path we created
+    -- file_stat should be non-nil, and stat_err should be nil on success
+    local file_stat, stat_err = os.Stat(path)
+    if stat_err ~= nil then
+        return false
+    elseif file_stat ~= nil then
+        -- Assume it exists if no errors
+        return file_stat:IsDir()
+    end
+    return false
+end
+
+
+function FindContent(str, searchLoc)
 
     local bp = micro.CurPane()
 
+    micro.InfoBar():Message("Find Content called")
+
     setupFzf(bp)
-    local selectedText = util.String(str)
+    -- local selectedText = util.String(str)
+    local selectedText = str
     local fzfArgs = ""
 
     -- micro.Log("selectedText before: ", selectedText)
     -- micro.Log("OmniContentArgs before: ", OmniContentArgs)
-    
-    local os = getOS()
-    if os == "Unix" then
+
+    local firstWord, otherWords = selectedText:match("^(.-)%s-(.*)$")
+
+    if firstWord == nil then
+        micro.InfoBar():Error("Failed to extract first word... str: ", str)
+        return
+    end
+
+    local currentOS = getOS()
+    if currentOS == "Unix" then
         selectedText = selectedText:gsub("'", "'\\''")
+        firstWord = firstWord:gsub("'", "'\\''")
         fzfArgs = OmniContentArgs:gsub("'", "'\\''")
     else
         selectedText = selectedText:gsub("'", '"')
+        firstWord = firstWord:gsub("'", '""')
         fzfArgs = OmniContentArgs:gsub("'", '"')
     end
 
-    -- micro.Log("selectedText after: ", selectedText)
-    -- micro.Log("OmniContentArgs after: ", fzfArgs)
 
-    local grepCmd = "grep -I -i -r -n \""..selectedText.."\" | "
-    local finalCmd = grepCmd..fzfCmd.." "..fzfArgs.." -q \""..selectedText.."\""
+    local finalCmd = "rg -i -uu -n \""..firstWord.."\" | "..fzfCmd.." "..fzfArgs.." -q \""..selectedText.."\""
 
-    if os == "Unix" then
+    if currentOS == "Unix" then
         finalCmd = "sh -c \'"..finalCmd.."\'"
     else
         finalCmd = "cmd /s /v /c "..finalCmd..""
@@ -101,7 +164,29 @@ function FindContent(str)
 
     -- micro.Log("Running search cmd: ", finalCmd)
 
+    local currentLoc = os.Getwd()
+
+    if searchLoc ~= nil and searchLoc ~= "" then
+        if not path_exists(searchLoc) then
+            micro.InfoBar():Error("", searchLoc, " doesn't exist")
+            return
+        end
+
+        if not IsPathDir(searchLoc) then
+            micro.InfoBar():Error("", searchLoc, " is not a directory")
+            return
+        end
+
+        bp:CdCmd({searchLoc})
+    end
+
+    micro.InfoBar():Message("finalCmd: ", finalCmd)
+
     local output, err = shell.RunInteractiveShell(finalCmd, false, true)
+
+    if searchLoc ~= nil and searchLoc ~= "" then
+        bp:CdCmd({currentLoc})
+    end
 
     if err ~= nil or output == "--" then
         -- micro.InfoBar():Error("Error is: ", err:Error())
@@ -110,40 +195,47 @@ function FindContent(str)
         -- lineNumber = tonumber(lineNumber)
         fzfParseOutput(filePath, bp, lineNumber)
     end
-
 end
 
 
 function fzfParseOutput(output, bp, lineNum)
-  if output ~= "" then
-    local file = string.gsub(output, "[\n\r]", "")
+    if output ~= "" then
+        local file = string.gsub(output, "[\n\r]", "")
     
-    if file == nil then
-        return
-    end
-
-    -- micro.InfoBar():Message("file is ", file)
-
-    if fzfOpen == "newtab" then
-       bp:NewTabCmd({file})
-    else
-        local buf, err = buffer.NewBufferFromFile(file)
-        if fzfOpen == "vsplit" then
-            bp:VSplitIndex(buf, true)
-        elseif fzfOpen == "hsplit" then
-            bp:HSplitIndex(buf, true)
-        else
-            bp:OpenBuffer(buf)
+        if file == nil then
+            return
         end
-    end
 
-    micro.CurPane():GotoCmd({lineNum})
-  end
+        -- micro.Log("fzfParseOutput starts")
+
+        -- micro.InfoBar():Message("file is ", file)
+
+        if fzfOpen == "newtab" then
+           bp:NewTabCmd({file})
+        else
+            local buf, err = buffer.NewBufferFromFile(file)
+            if fzfOpen == "vsplit" then
+                bp:VSplitIndex(buf, true)
+            elseif fzfOpen == "hsplit" then
+                bp:HSplitIndex(buf, true)
+            else
+                bp:OpenBuffer(buf)
+            end
+        end
+
+        -- micro.Log("fzfParseOutput new buffer")
+
+
+        micro.CurPane():GotoCmd({lineNum})
+
+        -- micro.Log("fzfParseOutput new line")
+    end
 end
 
 function OmniCenter(bp)
     local view = bp:GetView()
     bp.Cursor:ResetSelection()
+    bp.Buf:ClearCursors()
     bp.Cursor:GotoLoc(buffer.Loc(view.StartCol, view.StartLine.Line + view.Height / 2))
 end
 
@@ -179,6 +271,12 @@ function OmniSelect(bp, args)
 end
 
 
+function GoToPreviousHistory(bp)
+    -- TODO(NOW): Trverse the list
+
+end
+
+
 
 
 function TestECB(msg)
@@ -189,20 +287,48 @@ function TestDoneCB(msg, cancelled)
     micro.Log("TestDoneCB called with message ", msg, " and cancelled ", cancelled)
 end
 
-function OmniTest(bp)
-    micro.InfoBar():Prompt("Test prompt", "Test Message", "Test", TestECB, TestDoneCB)
+function onAnyEvent()
+    -- micro.Log("onAnyEvent called")
+    if micro.CurPane() == nil or micro.CurPane().Cursor == nil then
+        return
+    end
+    
+    local currentCursorLoc = micro.CurPane().Cursor.Loc
+    local bufPath = micro.CurPane().Buf.Path
+    
+    micro.Log("In ", bufPath, ":", currentCursorLoc.X, currentCursorLoc.Y)
+
+    -- TODO(NOW):   Check if the cursor is different. 
+    --              If so, check if it is because we are traversing in history. 
+    --                  If not, check if we are at the of the history
+    --                      If so, Just append the new history
+    --                      If not, remove the rest of the list items, then append
 
 
+    
+end
+
+function OmniTest(bp, args)
+    -- micro.InfoBar():Prompt("Test prompt", "Test Message", "Test", TestECB, TestDoneCB)
+    bp:CdCmd(args)
+end
+
+function OmniTest2(bp, args)
+    -- micro.InfoBar():Prompt("Test prompt", "Test Message", "Test", TestECB, TestDoneCB)
+    local wd = os.Getwd()
+
+    micro.InfoBar():Message("Getwd: ", wd)
 end
 
 
 function init()
     -- config.MakeCommand("fzfinder", fzfinder, config.NoComplete)
-    config.MakeCommand("OmniContent", OmniContent, config.NoComplete)
+    config.MakeCommand("OmniFind", OmniContent, config.NoComplete)
     config.MakeCommand("OmniCenter", OmniCenter, config.NoComplete)
-    config.MakeCommand("OmniSelect", OmniSelect, config.NoComplete)
+    config.MakeCommand("OmniJumpSelect", OmniSelect, config.NoComplete)
 
     config.MakeCommand("OmniTest", OmniTest, config.NoComplete)
+    config.MakeCommand("OmniTest2", OmniTest2, config.NoComplete)
 
   
 end
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Micro Omni
+# 🧰 Micro Omni
 
 Just a swiss army knife plugin that adds the functionalities I have from VSCodium.
 
@@ -12,30 +12,58 @@ List of features not in any particular order:
 - (WIP) Contect selection within brackets
 - (WIP) Diff view
 - (WIP) Copy current file path
-- (WIP) Resize split with keyboard
-<!-- Using https://github.com/zyedidia/micro/issues/1807#issuecomment-1907899274 -->
+- (WIP) Resize split with keyboard <!-- Using https://github.com/zyedidia/micro/issues/1807#issuecomment-1907899274 -->
 - (WIP) Minimap
 
-### Installation
+## 📦️ Installation
 This is still a WIP plugin so don't have any releases yet. To use this, you will need to clone it.
 
 `git clone https://github.com/Neko-Box-Coder/MicroOmni` to your micro `plug` directory
 
 
-### Requirements
-- micro (master or nightly)
+## 📐 Requirements
+- micro (from [my branch](https://github.com/Neko-Box-Coder/micro-dev) for now due to bug)
 - fzf
-- grep
-- (optional) bat
+- ripgrep
+- bat
 
 All of these are available for Unix and Windows
-> (WIP) Windows link to requirements
+> Windows link to requirements
+>
+> [https://github.com/junegunn/fzf/releases](https://github.com/junegunn/fzf/releases)
+>
+> [https://github.com/BurntSushi/ripgrep/releases](https://github.com/BurntSushi/ripgrep/releases)
+>
+> [https://github.com/sharkdp/bat/releases](https://github.com/sharkdp/bat/releases)
+
+## 🔍️ Finding files recursively with fzf
+
+To find a with keyword(s), launch command `OmniFind` which is bindable to a key.
+1. First it will ask you which directory to search from, if a relative path is give, 
+this will be relative to current working directory (not to be confused with current file directory).
+Leaving empty will default to current working directory.
+    - `{fileDir}` can be used to substitute the directory path of the current file. 
+2. If any text was selected prior to launching the `OmniFind` command, that text will be used
+for searching. If not, a prompt will ask you what to search.
+3. If successful, a fzf window will be launched. Here are the keybindings by default:
+    - up/down: Navigate results
+    - alt-up / alt-down: Navigate half page of results
+    - page-up / page-down: Scroll up and down for the preview window
+
+
+> [!CAUTION] 
+> The rest of this README is still work in progress and incomplete
+
+
 
 ### Keybindings/Commands
 - `OmniCenter`: Centers your cursor to the middle of the viewport
 - `OmniContent`: Searches all the file contents recursively with whatever is selected or entered
 - `OmniSelect`: Selects from current cursor position to either relative line or absolute line
     - This is meant to be used as a command, so bind it like this `"command-edit:OmniSelect "`
+
+
+
 
 
 ### Settings
@@ -48,9 +76,10 @@ All of these are available for Unix and Windows
 - `fzfCmd`: (Extending from fzfinder) The `fzf` location.
     - Defaults to `"fzf"`
 - `fzfOpen`: (Extending from fzfinder) How to open the new file. Available options are:
+    - `thispane`: (Default) Opens in current pane
     - `newtab`: Opens in new tab
     - `vsplit`: Opens in new pane as vertical split
     - `hsplit`: Opens in new pane as horizontal split
-    - `thispane`: (Default) Opens in current pane
-- `fzfpath`: The root path to search from, can be absolute path or relative to open file by setting to `relative`.
-    -If empty or not specified, defaults to directory where micro was launched
+
+<!-- - `fzfpath`: The root path to search from, can be absolute path or relative to open file by setting to `relative`. -->
+<!--     -If empty or not specified, defaults to directory where micro was launched -->
